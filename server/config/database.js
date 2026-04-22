159a421ef36badb5cee4b516da9cc457e846f1dc
amike42w-sys <amike42w@gmail.com>
2026-04-22 14:21:22 +0800

    fix: 调整数据库时区为北京时间并修复Promise异步Bug

--- server/config/database.js
+++ server/config/database.js
@@ -5,6 +5,12 @@ const sequelize = new Sequelize('certificate_db', 'certificate_db', '050309', {
   host: '127.0.0.1',
   dialect: 'mysql',
   logging: console.log, // 【必须开启】我们要看 SQL 日志
+  // --- 核心修改：加上下面这两行 ---
+  timezone: '+08:00', // 强制 Sequelize 使用东八区时间
+  dialectOptions: {
+    dateStrings: true, // 强制以字符串格式从数据库读取日期，防止时区二次转换
+    typeCast: true
+  }
 });
 
 // 还要加上这一行，因为 index.js 需要它
